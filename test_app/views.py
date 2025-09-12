a223c3471f9cef583cf9c02f8f12be19de1bd27f
KuriharaTakuya <kurihara@n-matrix.com>
2025-09-12 09:45:55 +0900

    add template

--- test_app/views.py
+++ test_app/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def index(request):
-    return render(request, 'index.html')
+    context = {
+
+    }
+    return render(request, 'index.html', context)
